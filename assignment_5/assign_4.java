e771c94c197196818406ffa90884e0b3e15def04
Nishant Nahar <187931098+Its-Nishant-10@users.noreply.github.com>
2026-01-13 13:20:28 +0530

    Update

--- assignment_5/assign_4.java
+++ assignment_5/assign_4.java
@@ -5,14 +5,14 @@ class A_0 {
     }
 }
 
-class B_0 extends A_0 {
-    B_0() {
+class assign_4 extends A_0 {
+    assign_4() {
         System.out.println("B");
         super();
     }
 
     public static void main(String[] args) {
         System.out.println("Nishant Nahar - 241551078");
-        new B_0();
+        new assign_4();
     }
 }
